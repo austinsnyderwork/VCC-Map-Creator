bee59f3387ac486bed7fd20c8f4cb546ee31ce65
austinsnyderwork <austin-snyder@uiowa.edu>
2024-08-30 16:22:57 -0500

    c

--- map_creation/map_creator.py
+++ map_creation/map_creator.py
@@ -1,8 +1,9 @@
 import geopy as gp
+import logging
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+logging.basicConfig(level=logging.INFO)
 
 vcc_file_path = "C:/Users/austisnyder/programming/programming_i_o_files/VccDump.xls"
 vcc_df = pd.read_excel(vcc_file_path, sheet_name='VccExcelDump')
@@ -10,18 +11,42 @@ vcc_df = pd.read_excel(vcc_file_path, sheet_name='VccExcelDump')
 to_cities = set(vcc_df['Community'])
 from_cities = set(vcc_df['Point of Origin'])
 
-# Find cities in 'Point of Origin' that are not in 'Community'
 from_cities_unique = from_cities - to_cities
-
-# Add unique 'from_cities' to 'to_cities'
 to_cities.update(from_cities_unique)
+cities = list(to_cities)
 
-# Convert back to list if needed
-to_cities = list(to_cities)
 gn = gp.geocoders.GeoNames(username='austinsnyder')
 city_coords = {}
 for city in cities:
-    location = gn.geocode(f"{city}, IA")
+    logging.info(f"Geocoding city: {city}")
+
+    # There's a comma if the state abbreviation is already included in the city name (ex: Omaha, NE)
+    city_name = city if ',' in city else f"{city}, IA"
+
+    location = gn.geocode(city_name, timeout=10)
     city_coords[city] = location.latitude, location.longitude
+
+row_header = ['city', 'latitude_from', 'longitude_from', 'to', 'latitude_to', 'longitude_to']
+
+
+def create_power_bi_row(row):
+    new_row = pd.Series([''] * len(row_header), index=row_header)
+    from_coord = city_coords[row['Point of Origin']]
+    to_coord = city_coords[row['Community']]
+
+    new_row['city'] = row['Point of Origin']
+    new_row['latitude_from'] = from_coord[0]
+    new_row['longitude_from'] = from_coord[1]
+    new_row['to'] = row['Community']
+    new_row['latitude_to'] = to_coord[0]
+    new_row['longitude_to'] = to_coord[1]
+
+    return new_row
+
+
+power_bi_df = vcc_df.apply(create_power_bi_row, axis=1)
+power_bi_df.to_csv("C:/Users/austisnyder/programming/programming_i_o_files/vcc_lines_power_bi.csv")
+
+
 
 
